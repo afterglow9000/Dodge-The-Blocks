cefbcdd9660948935c1d13e0d22f7480b5cacb39
B. Bueno <afterglow9000@gmail.com>
2020-01-26 05:52:29 -0700

    initial commit

---
+++ Assets/Scripts/GamingManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GamingManager : MonoBehaviour
+{
+    public float slowness = 10f;
+
+    public void EndGame ()
+    {
+        StartCoroutine(RestartLevel());
+    }
+
+    private IEnumerator RestartLevel()
+    {
+        Time.timeScale = 1f / slowness;
+        Time.fixedDeltaTime = Time.fixedDeltaTime / slowness;
+        yield return new WaitForSeconds(1f / slowness);
+        Time.timeScale = 1f;
+        Time.fixedDeltaTime = Time.fixedDeltaTime * slowness;
+        SceneManager.LoadScene(0);
+    }
+
+}
